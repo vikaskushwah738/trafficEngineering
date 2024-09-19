1c2b0560dc004ce79486e4e140269fde625c8953
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-09-20 01:22:21 +0530

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,13 @@ import { ObjectDetection } from "@/components/ObjectDetection";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col text-center p-8">
-      <h1 className="gradient-text font-medium text-2xl md:text-3xl lg:text-5xl tracking-tighter text-center">Vehicale traking And Classification count</h1>
-       <ObjectDetection/>
+    <div className="flex min-h-screen flex-col text-center p-8">
+      <h1
+        className="gradient-text font-extralight text-xl md:text-3xl
+         lg:text-5xl tracking-tighter text-center">
+        Vehicale traking And Classification count
+      </h1>
+      <ObjectDetection />
     </div>
   );
 }
